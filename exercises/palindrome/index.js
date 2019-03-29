4a02ccf1ae80445065ffb26583e8686ca2444699
Mallory <mallory.feuer@gmail.com>
2019-03-28 22:30:20 -0400

    palindrome solutions

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,14 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - 1 - i]
+  })
+}
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+//   return str.split('').reverse().join('') === str;
+// }
