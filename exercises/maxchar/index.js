5428ef29b555cd6647ff78c032117565c6fdc3a0
Mallory <mallory.feuer@gmail.com>
2019-04-05 15:51:15 -0400

    solution for maxChar problem- hashmaps!

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let hashmap = {};
+  let max = 0;
+  let maxChar = "";
+
+   for (let i = 0; i < str.length; i++) {
+     hashmap[str[i]] = hashmap[str[i]] + 1 || 1
+   }
+
+   for (let key in hashmap) {
+     if (hashmap[key] > max) {
+       max = hashmap[key];
+       maxChar = key;
+     }
+   }
+
+   return maxChar;
+}
 
 module.exports = maxChar;
